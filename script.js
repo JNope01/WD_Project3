4dc93f4896646e422581c6e98623f922c8b3090f
JeffDing27 <rud300@ensign.edu>
2023-12-12 20:35:42 -0700

    Jeff

--- script.js
+++ script.js
@@ -24,15 +24,21 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  if (slides.length > 0 && slideIndex >= 0 && slideIndex < slides.length) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slides[slideIndex].style.display = "block";  
+  dots[slideIndex].className += " active";
+  // Increment slideIndex or reset to 0
+  slideIndex = (slideIndex + 1) % slides.length;
+  
 }
+setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+ 
+
